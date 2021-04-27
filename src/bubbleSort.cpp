0e7621be7d9cd338560662193822571290f41b95
LoganLupe3 <LoganLupeamanu@my.unt.edu>
2021-04-27 14:30:34 -0500

    Added sorting of rectangles

--- src/bubbleSort.cpp
+++ src/bubbleSort.cpp
@@ -5,6 +5,7 @@
 */
 
 #include <iostream>
+#include <unistd.h>
 #include <vector>
 #include <iterator>
 #include <SFML/Graphics.hpp>
@@ -15,11 +16,19 @@ int *nums; //These numbers will determine the height of the rectangles that will
 int size = 100;
 int gap = 4;
 
-void swap(int i, int j, int x, int y){
+void swap(int i, int j, sf::RectangleShape *rectangles){
+    rectangles[i].setFillColor(sf::Color::Green);
+    rectangles[j].setFillColor(sf::Color::Green);
 
+    sf::Vector2f temp = rectangles[i].getPosition();
+    rectangles[i].setPosition(rectangles[j].getPosition());
+    rectangles[j].setPosition(temp);
+
+    rectangles[i].setFillColor(sf::Color::White);
+    rectangles[j].setFillColor(sf::Color::White);    
 }
 
-void bubbleSort(){
+void bubbleSort(sf::RectangleShape *rectangles){
     int temp, i, j;
     
     for(int i = 0; i < size; i++){
@@ -28,25 +37,29 @@ void bubbleSort(){
                 temp = nums[j];
                 nums[j] = nums[j+1];
                 nums[j+1] = temp;
+
+                swap(i, j, rectangles);
             }
         }
     }
 }
 
 int main(){
+    int height = 600;
+    int width = 800;
     //Allocate memory to nums
     nums = new int[size];
 
     sf::RectangleShape rectangles[size];
 
     //Create the window
-    sf::RenderWindow window(sf::VideoMode(800,600), "test");
+    sf::RenderWindow window(sf::VideoMode(width,height), "test");
 
     srand(time(NULL));
 
-    //Initialize array with random values (1-100 inclsive)
+    //Initialize array with random values (1-799 inclsive)
     for(int i = 0; i < size; i++){
-        nums[i] = rand() % 100 + 1;
+        nums[i] = rand() % 599 + 1;
     }
     
 
@@ -56,6 +69,7 @@ int main(){
     }
     printf("\n");
 
+    /*
     bubbleSort();
 
     printf("Sorted:\n");
@@ -63,6 +77,9 @@ int main(){
         printf("%d ", nums[i]);
     }
     printf("\n");
+    */
+    
+    usleep(200000);
 
     while(window.isOpen()){
         sf::Event e;
@@ -72,14 +89,32 @@ int main(){
                 window.close();
         }
 
-        //window.clear();
+        window.clear();
         //Draw rectangles
+        bubbleSort(rectangles);
+        int i, l;
+        while (l < sizeof(rectangles) / sizeof(rectangles[0])) {
+            rectangles[l].setSize(sf::Vector2f(3, nums[l]));
+
+            rectangles[l].setPosition(i, height-599);
+
+            window.draw(rectangles[l]);
+
+            i += 7;
+
+            l++;
+        }
+        i = 0;
+        l = 0;
+
+        /*
         for(int i = 0; i < size; i++){
             rectangles[i].setSize(sf::Vector2f(3.69, nums[i]));
             rectangles[i].setFillColor(sf::Color::White);
             rectangles[i].setPosition((i*7.69), 600);
             window.draw(rectangles[i]);
         }
+        */
 
         window.display();
     }
